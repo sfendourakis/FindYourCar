8b222a039c44a28be4600d4c7aa12f0a59e337af
Athanasopoulos K. Panagiotis <74834525+a8anpan@users.noreply.github.com>
2021-06-11 18:54:35 +0300

    Add files via upload

---
+++ seller.py
@@ -0,0 +1,7 @@
+import user
+
+class seller(user):
+    def __init__(self, name, email, password, id, role):
+        super().__init__(name, email, password, id, role)
+
+
---
+++ user.py
@@ -0,0 +1,8 @@
+class user:
+    def __init__(self, name, email, password, id,role):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.id = id
+        self.role = role
+
